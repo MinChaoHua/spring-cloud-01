185f340f0721fd5f904c8385dc2462d25751f2c1
chaohua <minchaohua@smuratec.com>
2019-05-06 17:43:44 +0800

    添加 ribbon

--- src/main/java/com/system/DemoApplication.java
+++ src/main/java/com/system/DemoApplication.java
@@ -2,13 +2,21 @@ package com.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
 @EnableEurekaServer
 public class DemoApplication {
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
